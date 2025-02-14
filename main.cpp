a2d8e18037ce0f4fdd0d13491bdf67b18d30fbcc
xolikov <a.xolikov@newuu.uz>
2025-02-14 22:32:24 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Human is not a Ponginae";
+    cout << endl;
+    cout << "Human is a Homininae";
+    cout << endl;
+    cout << "Gorilla is a Hominidae";
+    cout << endl;
+    cout << "Gorilla is not a Hominini";
+    cout << endl;
+    cout << "Pongo is a Hominidae";
+    cout << endl;
+    cout << "Pongo is not a Gorillini";
+    cout << endl;
+    cout << "Hominidae is not a Human";
+    cout << endl;
+    cout << "Hominidae is not a Gorilla";
+    cout << endl;
+    return 0;
+}
